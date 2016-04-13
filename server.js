03d411b4634f6cb9ef7c659e70db88ad2762a534
Jason Cheng <yannru.cheng@gmail.com>
2016-04-13 21:51:13 +0000

    set up listeners. need to write unit tests

--- server.js
+++ server.js
@@ -1,82 +1,94 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
+// Create references for library
 var http = require('http');
 var path = require('path');
-
-var async = require('async');
-var socketio = require('socket.io');
 var express = require('express');
+var Firebase = require('firebase');
+var config = require('./helper/config.js');
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
+// Express server setup
 var router = express();
 var server = http.createServer(router);
-var io = socketio.listen(server);
 
-router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
+// Authenticate Firebase.
+var firebaseRef = new Firebase(config.firebase.url);
+firebaseRef.authWithCustomToken(config.firebase.secret, function(error, authData) {
+  if (error) {
+    console.log("Firebase server authentication failed.");
+  } else {
+    console.log("Authenticated with Firebase secret successfully.");
+  }
+});
 
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
 
-    sockets.push(socket);
+// activity added listener
+var activitiesRef = firebaseRef.child("activities");
 
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
+activitiesRef.on("child_added", function(snapshot){
+  var activity = snapshot.val();
+  
+  // get userId from activity.
+  var userId = activity.userId;
+  console.log(activity);
+  
+  // get live challenges from user
+  var liveChallengeRef = new Firebase(config.firebase.url + "/live/" + userId + "/active")
+  liveChallengeRef.once("value").then(function(liveChallenges){
+    // go through all live active challenges
+    liveChallenges.forEach(function(challenge) {
+      console.log(challenge.key());
+      
+      // get info of the challenge
+      var publicChallengeRef = new Firebase(config.firebase.url + "/challenges/" + challenge.key());
+      publicChallengeRef.once("value", function(snapshot){
+        var challenge = snapshot;
+        
+         // compare activity with challenge
+        // activity startDate > challenge startDate
+        if (activity.startDate > challenge.startDate) {
+          // decides if challenge progress should be updated with the activity distance or pace
+          var progress = challenge.progress
+          
+          // 1v1
+          if (challenge.type == "OneVOne"){
+            switch(challenge.mode){
+              case "Distance":
+                  progress[userId] = activity.distance
+                  
+                break
+              case "Speed":
+                break
+              case "Streak":
+                break
+            }
+          }
+          // coop
+          else if (challenge.type == "Coop"){}
+          
+          challenge.progress = progress;
+          // update challenge
+          publicChallengeRef.update(challenge);
+        }
       });
     });
   });
+});
 
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
 
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
+  
+// challenge updated listener
+var publicChallengeRef = new Firebase(config.firebase.url + "/challenges")
+publicChallengeRef.on("child_changed", function(snapshot){
+  var challenge = snapshot.val()
+  console.log("challenge progress of challenge: " + snapshot.key() + "has been updated")
+  
+  // check challenge update with challenge completed condition
+  
+  // if completed... move challenge from active to completed table for both host and member
+});
+
+
+
+
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
