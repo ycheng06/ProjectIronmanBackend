bae5dc58cd4c010862a6726d565cbcfb044dd307
Jason Cheng <yannru.cheng@gmail.com>
2016-04-15 19:34:16 +0000

    teseting listeners and fixing table names

--- server.js
+++ server.js
@@ -19,32 +19,37 @@ firebaseRef.authWithCustomToken(config.firebase.secret, function(error, authData
   }
 });
 
-
-// activity added listener
-var activitiesRef = firebaseRef.child("activities");
+// ============== Activity ========================
+// ** Public activity child added listener
+var activitiesRef = firebaseRef.child("public_activities");
 
 activitiesRef.on("child_added", function(snapshot){
   var activity = snapshot.val();
   
   // get userId from activity.
   var userId = activity.userId;
-  console.log(activity);
+  console.log("activity id " + snapshot.key());
   
   // get live challenges from user
-  var liveChallengeRef = new Firebase(config.firebase.url + "/live/" + userId + "/active")
+  var liveChallengeRef = new Firebase(config.firebase.url + "/live_challenges/" + userId + "/active");
   liveChallengeRef.once("value").then(function(liveChallenges){
     // go through all live active challenges
     liveChallenges.forEach(function(challenge) {
-      console.log(challenge.key());
+      console.log("live challenge id " + challenge.key());
       
       // get info of the challenge
-      var publicChallengeRef = new Firebase(config.firebase.url + "/challenges/" + challenge.key());
+      var publicChallengeRef = new Firebase(config.firebase.url + "/public_challenges/" + challenge.key());
       publicChallengeRef.once("value", function(snapshot){
-        var challenge = snapshot;
-        
+        var challenge = snapshot.val();
+        console.log("challenge found");
          // compare activity with challenge
+         
         // activity startDate > challenge startDate
+        console.log("activity start date: " + activity.startDate);
+        console.log("challenge start date: " + challenge.startDate);
+        
         if (activity.startDate > challenge.startDate) {
+          console.log("activity date counts");
           // decides if challenge progress should be updated with the activity distance or pace
           var progress = challenge.progress
           
@@ -73,22 +78,27 @@ activitiesRef.on("child_added", function(snapshot){
   });
 });
 
+// ==========================================================
 
-  
-// challenge updated listener
-var publicChallengeRef = new Firebase(config.firebase.url + "/challenges")
+
+// ================== Public Challenge =============================  
+// ** Public challenge child changed listener
+var publicChallengeRef = new Firebase(config.firebase.url + "/public_challenges")
 publicChallengeRef.on("child_changed", function(snapshot){
   var challenge = snapshot.val()
-  console.log("challenge progress of challenge: " + snapshot.key() + "has been updated")
+  console.log("challenge progress of challenge: " + snapshot.key() + " has been updated")
   
   // check challenge update with challenge completed condition
   
   // if completed... move challenge from active to completed table for both host and member
 });
 
+// ** Public challenge child added listener
+publicChallengeRef.on("child_added", function(snapshot){
+  console.log(snapshot.val())  
+});
 
-
-
+// ==========================================================
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
